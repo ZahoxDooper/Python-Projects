21882f5a4e571edc24e31fdcbbe35a4bdfb8819c
ZahoxDooper <hamzaalshamekh@gmail.com>
2025-08-18 19:05:34 +0300

    Add files via upload

---
+++ pythonprojects.py
@@ -0,0 +1,44 @@
+print("Welcome to the Ohm's law calculator! ")
+print("What do you want to calculate?")
+print("V = Voltage" , "I = Current" , "R = Resistance")
+choice = input("Enter your choice (V/I/R) :")
+if choice == "V":
+    I = float(input("Current(I) : "))
+    R = float(input("Resistance(R) : "))
+    print("V =" , I * R , "Volts")
+elif choice == "I":
+    V = float(input("Voltage(V) : "))
+    R = float(input("Resistance(R) : "))
+    print("I =" , V / R , "Amps")
+elif choice == "R":
+    V = float(input("Voltage(V) : "))
+    I = float(input("Current(I) : "))
+    print("R =" , V / R , "Ohms")
+    print("Chatgpt is a good teacher")
+    
+    
+              
+    
+    
+
+
+    
+
+    
+    
+    
+
+            
+
+
+
+
+                      
+
+           
+           
+
+
+
+
+
